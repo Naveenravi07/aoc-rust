ccdaaa4310ae0ba17541b546d8a4190a23363693
Naveenravi07 <gamerkidnav@gmail.com>
2023-12-08 21:27:39 +0530

    day1p2

---
+++ day-01/day1p2/target/debug/deps/day1p2-5b6dac1eb1e6ec94.d
@@ -0,0 +1,5 @@
+/home/shastri/deez/aoc/day-01/day1p2/target/debug/deps/day1p2-5b6dac1eb1e6ec94.rmeta: src/main.rs
+
+/home/shastri/deez/aoc/day-01/day1p2/target/debug/deps/day1p2-5b6dac1eb1e6ec94.d: src/main.rs
+
+src/main.rs:
---
+++ day-01/day1p2/target/debug/deps/day1p2-8252b7d348f07014.d
@@ -0,0 +1,5 @@
+/home/shastri/deez/aoc/day-01/day1p2/target/debug/deps/day1p2-8252b7d348f07014: src/main.rs
+
+/home/shastri/deez/aoc/day-01/day1p2/target/debug/deps/day1p2-8252b7d348f07014.d: src/main.rs
+
+src/main.rs:
